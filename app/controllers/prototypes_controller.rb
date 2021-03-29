e2f2ec920540607cd844c979aef2263cddf5784e
nekomaru14 <eagle.h.j.ss@gmail.com>
2021-03-29 19:02:54 +0900

    エラー解消追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :new]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
